d0ad69dd10cf3344f3fd75e89c3571b2aa5882cd
soumyadeep <soumyadeepsen97@gmail.com>
2025-01-31 13:53:57 +0530

    added pattern

--- pattern8.cpp
+++ pattern8.cpp
@@ -1,16 +1,21 @@
 #include <iostream> 
 using namespace std ;
 int main () {
-    cout << "this pattern is flyod triangle pattern  like \n 1 \n 21 \n 321 \n" ;
+    cout << "this pattern is inverted triangle" ;
     cout << "Enter the number " ;
     int  n ; 
     cin >> n ; 
     int num = 1;
     for ( int i =0 ; i <n ;i++){
-        for(int j =i+1 ; j>0 ; j--)
+        for(int j =0 ; j<=i ; j++)
         {
-            cout << num << " ";
-            num++;
+            cout << " ";
+           
+        }
+        for(int j =n ; j>i ; j--)
+        {
+            cout << "*";
+           
         }
         cout << endl ;
         
